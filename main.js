b964e3eeeabd909a0a5a69bf12cbdfec1a11f843
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-01-18 23:18:48 +0100

    menu overflow

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const menuToggle = document.querySelector('.menu-toggle')
 const mobileMenu = document.querySelector('.mobile-menu')
 const closeMenu = document.querySelector('.close-menu')
+const overflow = document.querySelector('.overflow')
 
 menuToggle.addEventListener('click', () => {
     console.log('you clicked on toggle')
     mobileMenu.classList.add('open-menu')
+    overflow.style.transform = 'translateX(0)'
 })
 
 closeMenu.addEventListener('click', () => {
     console.log('you clicked on close menu')
     mobileMenu.classList.remove('open-menu')
+    overflow.style.transform = 'translateX(-150px)'
 })
